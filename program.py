5f745ba9242813312d8f363c4726611bfda54947
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2021-11-30 18:27:49 +0200

    done

---
+++ program.py
@@ -0,0 +1,9 @@
+
+from employee import Employee
+from worker import Worker
+from manager import Manager
+
+employee1 = Worker(123, 'liza', 'tel aviv', 24, 55)
+print(employee1)
+employee1.calculate_salary(24, 56)
+
--- worker.py
+++ worker.py
@@ -1,16 +1,15 @@
-from abc import ABC
 
 from employee import Employee
 
-class Worker(Employee, ABC):
-def __init__(self, name, address, age, hours_per_day, hour_rate):
-    super(self.__class__, self).__init__(self, name, address, age)
+class Worker(Employee):
+    def __init__(self, name, address, age, hours_per_day, hour_rate):
+        super(self.__class__, self).__init__(self, name, address, age)
 
-def calculate_salary(self):
-    super(self.__class__, self)
+    def calculate_salary(self):
+        super(self.__class__, self)
 
-def __str__(self):
-    return super.__str__()
+    def __str__(self):
+        return super.__str__()
 
 
 
